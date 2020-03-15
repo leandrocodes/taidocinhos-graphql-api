267d6c16a00fc3063067b142fc490dbb89b5e798
leandrocodes <leandrovianacodes@gmail.com>
2020-03-15 18:08:20 -0400

    user registration

--- src/graphql/resolvers/userResolvers.js
+++ src/graphql/resolvers/userResolvers.js
@@ -1,3 +1,7 @@
+const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
+
+const { secretKey } = require('../../config')
 const User = require('../../models/User')
 
 module.exports = {
@@ -12,10 +16,34 @@ module.exports = {
     }
   },
   Mutation: {
-    register(_, args, ctx, info) {
+    async register(
+      _,
+      { registerInput: { username, email, password, confirmPassword } },
+      ctx,
+      info
+    ) {
       // TODO: Validate user data
       // TODO: Make sure user is unique
-      // TODO: Hash password and auth token
+      password = await bcrypt.hash(password, 12)
+      const user = new User({
+        email,
+        username,
+        password,
+        createdAt: new Date().toISOString()
+      })
+      const res = await user.save()
+
+      const token = jwt.sign({
+        id: res.id,
+        email: res.email,
+        username: res.username
+      }, secretKey, { expiresIn: `1h` })
+
+      return {
+        ...res._doc,
+        id: res._id,
+        token
+      }
     }
   }
 }
