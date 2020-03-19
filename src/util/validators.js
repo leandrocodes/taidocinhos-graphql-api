d3541775b9c7cb5bea92b47d86e83bfe9022af42
leandrocodes <leandrovianacodes@gmail.com>
2020-03-19 19:04:54 -0400

    validate user // not tested yet

--- src/util/validators.js
+++ src/util/validators.js
@@ -11,15 +11,30 @@ module.exports.validateRegisterInput = (username, email, password, confirmPasswo
   } else if (confirmPassword != password) {
     errors.password = 'As senhas não coincidem'
     errors.confirmPassword = 'As senhas não coincidem'
-  }
-  else {
+  } else {
     const regex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
     if (!email.match(regex)) {
-        errors.email = 'Você não inseriu um endereço de email válido.'
+      errors.email = 'Você não inseriu um endereço de email válido.'
     }
   }
   return {
-      errors,
-      valid: Object.keys(errors).length < 1
+    errors,
+    valid: Object.keys(errors).length < 1
+  }
+}
+
+module.exports.validateLoginInput = (email, password) => {
+  const errors = {}
+  if (email.trim() === '') errors.email = 'Email não pode ser vazio.'
+  if (password === '') errors.password = 'A senha não pode ser vazia.'
+  else {
+    const regex = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/
+    if (!email.match(regex)) {
+      errors.email = 'Você não inseriu um endereço de email válido.'
+    }
+  }
+  return {
+    errors,
+    valid: Object.keys(errors).length < 1
   }
 }
